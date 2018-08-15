50f1aa02e70a07455cab8fc37ad546f9e18cae3e
saurabh-solkar16 <saurabhsolkar4@gmail.com.com>
2018-08-15 18:26:58 +0530

    first

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>baithak dhaba</title>
+
+	 <link rel="stylesheet" href="http://localhost:8080/bootstrap/css/bootstrap.min.css">
+	<script src="http://localhost:8080/bootstrap/js/bootstrap.min.js" ></script>
+	
+	<script src="http://localhost:8080/jaquery/jquery-3.2.1.min.js"></script>
+<style type="text/css">
+	
+h1{
+	font-family: Courier;
+	letter-spacing: 2px;
+}
+
+</style>
+</head>
+<body>
+<div class="container">
+<h1>Gold and chariout</h1>
+
+<ul>
+	<<a href="">
+</ul>
+
+</div>
+</body>
+</html>
